24859ac19eb440f1917ab37af9285f0e44eea160
lananhhoang88 <67369919+lananhhoang88@users.noreply.github.com>
2022-06-07 22:06:20 +0700

    dstt

--- src/app/components/danhsachtrungtuyen/danhsachtrungtuyen-form/danhsachtrungtuyen-form.component.ts
+++ src/app/components/danhsachtrungtuyen/danhsachtrungtuyen-form/danhsachtrungtuyen-form.component.ts
@@ -1,12 +1,21 @@
 import { Component, Injector, OnInit } from '@angular/core';
 import { FormState, Operator } from 'src/app/shared/models/enums';
-import { DateTimeControlSchema, DropdownControlSchema, TabViewData, TextAreaControlSchema, TextControlSchema } from 'src/app/shared/models/schema';
+import { DateTimeControlSchema, DropdownControlSchema, MaskControlSchema, TabViewData, TextAreaControlSchema, TextControlSchema } from 'src/app/shared/models/schema';
 import { FormBase } from '../../../shared/base-class/form-base';
 import { DM_CoSoDaoTaoService } from '../../dm-cosodaotao/services/dm-cosodaotao.service';
+import { DM_DoiTuongDaoTaoService } from '../../dm-doituongdaotao/services/dm-doituongdaotao.service';
+import { DM_DoiTuongTuyenSinhService } from '../../dm-doituongtuyensinh/services/dm-doituongtuyensinh.service';
+import { DM_DoiTuongUuTienService } from '../../dm-doituonguutien/services/dm-doituonguutien.service';
 import { DM_DonViLienKetService } from '../../dm-donvilienket/services/dm-donvilienket.service';
 import { DM_GioiTinhService } from '../../dm-gioitinh/services/dm-gioitinh.service';
+import { DM_HanhKiemService } from '../../dm-hanhkiem/services/dm-hanhkiem.service';
 import { DM_HeDaoTaoService } from '../../dm-hedaotao/services/dm-hedaotao.service';
+import { DM_HocLucService } from '../../dm-hocluc/services/dm-hocluc.service';
+import { DM_HtTuyenSinhService } from '../../dm-httuyensinh/services/dm-httuyensinh.service';
 import { DM_KhoaHocService } from '../../dm-khoahoc/services/dm-khoahoc.service';
+import { DM_KhuVucService } from '../../dm-khuvuc/services/dm-khuvuc.service';
+import { DM_NganhService } from '../../dm-nganh/services/dm-nganh.service';
+import { DotNhapHocService } from '../../dotnhaphoc/services/dotnhaphoc.service';
 import { AddressService } from '../../user/services/address.service';
 import { DanTocService } from '../../user/services/dantoc.service';
 import { QuocTichService } from '../../user/services/quoctich.service';
@@ -24,12 +33,20 @@ export class DanhSachTrungTuyenFormComponent extends FormBase implements OnInit 
   constructor(
     injector: Injector,
     private _DanhSachTrungTuyenService: DanhSachTrungTuyenService,
-    private _dm_DotNhapHocService: DM_HeDaoTaoService,
+    private _dm_DotNhapHocService: DotNhapHocService,
     private _dm_QuocTichService: QuocTichService,
     private _dm_DanTocService: DanTocService,
     private _dm_GioiTinhService: DM_GioiTinhService,
     private _addressService: AddressService,
-    private _tongiaoService: ReligionService,
+    private _DM_NganhService: DM_NganhService,
+    private _DM_KhuVucService: DM_KhuVucService,
+    private _DM_HtTuyenSinhService: DM_HtTuyenSinhService,
+    private _DM_DoiTuongTuyenSinhService: DM_DoiTuongTuyenSinhService,
+    private _DM_DoiTuongUuTienService: DM_DoiTuongUuTienService,
+    private _DM_DoiTuongDaoTaoService: DM_DoiTuongDaoTaoService,
+    private _DM_HocLucService: DM_HocLucService,
+    private _DM_HanhKiemService: DM_HanhKiemService,
+
   ) {
     super(injector);
   }
@@ -38,8 +55,8 @@ export class DanhSachTrungTuyenFormComponent extends FormBase implements OnInit 
     this.setting.service = this._DanhSachTrungTuyenService;
     this.setting.schema = [
       new DropdownControlSchema({
-        field: 'idHeDaoTao',
-        label: 'Hệ đào tạo',
+        field: 'idDotNhapHoc',
+        label: 'Đợt nhập học',
         service: this._dm_DotNhapHocService,
         required: true
       }),
@@ -129,31 +146,127 @@ export class DanhSachTrungTuyenFormComponent extends FormBase implements OnInit 
           this.newBindingFilter('parentId', Operator.equal, 'idHuyen')
         ]
       }),
-      new TextAreaControlSchema({
+      new TextControlSchema({
         field: 'email',
         label: 'Email',
       }),
       new DropdownControlSchema({
         field: 'idNganhDangKy',
-        label: 'Tôn giáo',
-        service: this._tongiaoService
+        label: 'Ngành đăng ký',
+        service: this._DM_NganhService,
+        required: true
+      }),
+      new DropdownControlSchema({
+        field: 'idNganhTrungTuyen',
+        label: 'Ngành trúng tuyển',
+        service: this._DM_NganhService,
+        required: true
+      }),
+      new MaskControlSchema({
+        field: 'diemMon1',
+        label: 'Điểm môn 1'
+      }),
+      new MaskControlSchema({
+        field: 'diemMon2',
+        label: 'Điểm môn 2'
+      }),
+      new MaskControlSchema({
+        field: 'diemMon3',
+        label: 'Điểm môn 3'
+      }),
+      new MaskControlSchema({
+        field: 'diemCong',
+        label: 'Điểm cộng'
+      }),
+      new MaskControlSchema({
+        field: 'tongDiem',
+        label: 'Tổng điểm'
+      }),
+      new TextControlSchema({
+        field: 'toHopMon',
+        label: 'Tổ hợp môn'
+      }),
+      new DropdownControlSchema({
+        field: 'khuVuc',
+        label: 'Cơ sở đào tạo',
+        service: this._DM_KhuVucService
+      }),
+      new TextControlSchema({
+        field: 'soBaoDanh',
+        label: 'Số báo danh'
+      }),
+      new DropdownControlSchema({
+        field: 'idHtTuyenSinh',
+        label: 'Hình thức tuyển sinh',
+        service: this._DM_HtTuyenSinhService,
+        required: true
+      }),
+      new DropdownControlSchema({
+        field: 'idDoiTuongTuyenSinh',
+        label: 'Đối tượng tuyển sinh',
+        service: this._DM_DoiTuongTuyenSinhService,
+        required: true
+      }),
+      new DropdownControlSchema({
+        field: 'idDoiTuongUuTien',
+        label: 'Đối tượng ưu tiên',
+        service: this._DM_DoiTuongUuTienService
+      }),
+      new DropdownControlSchema({
+        field: 'idDoiTuongDaoTao',
+        label: 'Đối tượng đào tạo',
+        service: this._DM_DoiTuongDaoTaoService,
+        required: true
       }),
       new DateTimeControlSchema({
-        field: 'timeKt',
-        label: 'Thời gian kết thúc'
+        field: 'ngayQdTrungTuyen',
+        label: 'Ngày quyết định trúng tuyển',
+        width: 6
+      }),
+      new DateTimeControlSchema({
+        field: 'timeNhapHoc',
+        label: 'Thời gian nhập học',
+        width: 6
+      }),
+      new TextControlSchema({
+        field: 'diaDiemNhapHoc',
+        label: 'Địa điểm nhập học'
+      }),
+      new TextControlSchema({
+        field: 'maTinhLop12',
+        label: 'Mã tỉnh lớp 12'
+      }),
+      new TextControlSchema({
+        field: 'maTruongLop12',
+        label: 'Mã trường lớp 12'
+      }),
+      new MaskControlSchema({
+        field: 'thuTuNv',
+        label: 'Thứ tự nguyện vọng'
       }),
       new DropdownControlSchema({
-        field: 'idCoSoDaoTao',
-        label: 'Cơ sở đào tạo'
+        field: 'hocLucTHPT',
+        label: 'Học lực THPT',
+        service: this._DM_HocLucService,
       }),
       new DropdownControlSchema({
-        field: 'idDonViLienKet',
-        label: 'Đơn vị liên kết'
+        field: 'hanhKiemTHPT',
+        label: 'Hạnh kiểm',
+        service: this._DM_HanhKiemService,
       }),
-      new DropdownControlSchema({
-        field: 'trangThai',
-        label: 'Trạng thái',
-        dataSource: DataSourceTrangThai
+      new TextControlSchema({
+        field: 'diaChiNhanGiayBao',
+        label: 'Địa chỉ nhận giấy báo'
+      }),
+      new TextControlSchema({
+        field: 'soQD',
+        label: 'Số quyết định'
+      }),
+      new TextControlSchema({
+        field: 'maSV',
+        label: 'Mã sinh viên',
+        width: 6,
+        required: true
       }),
       new TextAreaControlSchema({
         field: 'ghiChu',
--- src/app/components/danhsachtrungtuyen/danhsachtrungtuyen.component.ts
+++ src/app/components/danhsachtrungtuyen/danhsachtrungtuyen.component.ts
@@ -1,11 +1,25 @@
 import { Component, Injector, OnInit } from '@angular/core';
-import { ColumnSchema, DropdownControlSchema, TabViewData } from '../../shared/models/schema';
+import { ColumnSchema, TabViewData } from '../../shared/models/schema';
 import { ListBase } from '../../shared/base-class/list-base';
 import { DanhSachTrungTuyenService } from './services/danhsachtrungtuyen.service';
 import { DataType, Operator } from 'src/app/shared/models/enums';
 import { DM_KhoaHocService } from '../dm-khoahoc/services/dm-khoahoc.service';
 import { GridInfo } from 'src/app/shared/models/grid-info';
 import { DataSourceTrangThai } from './models/const';
+import { DM_HeDaoTaoService } from '../dm-hedaotao/services/dm-hedaotao.service';
+import { QuocTichService } from '../user/services/quoctich.service';
+import { DanTocService } from '../user/services/dantoc.service';
+import { DM_GioiTinhService } from '../dm-gioitinh/services/dm-gioitinh.service';
+import { AddressService } from '../user/services/address.service';
+import { DM_NganhService } from '../dm-nganh/services/dm-nganh.service';
+import { DM_KhuVucService } from '../dm-khuvuc/services/dm-khuvuc.service';
+import { DM_HtTuyenSinhService } from '../dm-httuyensinh/services/dm-httuyensinh.service';
+import { DM_DoiTuongTuyenSinhService } from '../dm-doituongtuyensinh/services/dm-doituongtuyensinh.service';
+import { DM_DoiTuongUuTienService } from '../dm-doituonguutien/services/dm-doituonguutien.service';
+import { DM_DoiTuongDaoTaoService } from '../dm-doituongdaotao/services/dm-doituongdaotao.service';
+import { DM_HocLucService } from '../dm-hocluc/services/dm-hocluc.service';
+import { DM_HanhKiemService } from '../dm-hanhkiem/services/dm-hanhkiem.service';
+import { DotNhapHocService } from '../dotnhaphoc/services/dotnhaphoc.service';
 @Component({
   selector: 'danhsachtrungtuyen',
   templateUrl: './danhsachtrungtuyen.component.html',
@@ -15,56 +29,87 @@ export class DanhSachTrungTuyenComponent extends ListBase implements OnInit {
   constructor(
     injector: Injector,
     private _DanhSachTrungTuyenService: DanhSachTrungTuyenService,
-    private _dm_KhoaHocService: DM_KhoaHocService
+    private _dm_DotNhapHocService: DotNhapHocService,
+    private _dm_QuocTichService: QuocTichService,
+    private _dm_DanTocService: DanTocService,
+    private _dm_GioiTinhService: DM_GioiTinhService,
+    private _addressService: AddressService,
+    private _DM_NganhService: DM_NganhService,
+    private _DM_KhuVucService: DM_KhuVucService,
+    private _DM_HtTuyenSinhService: DM_HtTuyenSinhService,
+    private _DM_DoiTuongTuyenSinhService: DM_DoiTuongTuyenSinhService,
+    private _DM_DoiTuongUuTienService: DM_DoiTuongUuTienService,
+    private _DM_DoiTuongDaoTaoService: DM_DoiTuongDaoTaoService,
+    private _DM_HocLucService: DM_HocLucService,
+    private _DM_HanhKiemService: DM_HanhKiemService,
   ) {
     super(injector);
   }
 
   override ngOnInit(): void {
-    this.setting.objectName = 'đợt nhập học';
+    this.setting.objectName = 'trúng tuyển';
     this.setting.service = this._DanhSachTrungTuyenService;
-    this.setting.popupSize.width = 1100;
-    this.setting.popupSize.height = 650;
+    this.setting.popupSize.width = 1800;
+    this.setting.popupSize.height = 800;
     this.setting.cols = [
       new ColumnSchema({
-        field: 'idKhoaHoc',
-        label: 'Khóa học',
-        service: this._dm_KhoaHocService
+        field: 'maNhapHoc',
+        label: 'Mã nhập học',
       }),
       new ColumnSchema({
-        field: 'ma',
-        label: 'Mã đợt',
-        fullTextSearch: true
+        field: 'ho',
+        label: 'Họ',
       }),
       new ColumnSchema({
         field: 'ten',
-        label: 'Tên đợt',
-        fullTextSearch: true,
+        label: 'Tên',
+
       }),
       new ColumnSchema({
-        field: 'timeBd',
-        label: 'Bắt đầu',
-        dataType: DataType.date
+        field: 'ngaySinh',
+        label: 'Ngày sinh',
       }),
       new ColumnSchema({
-        field: 'timeKt',
-        label: 'Kết thúc',
-        dataType: DataType.date
+        field: 'gioiTinh',
+        label: 'Giới tính',
+        service: this._dm_GioiTinhService
       }),
       new ColumnSchema({
-        field: 'trangThai',
-        label: 'Trạng thái',
-        dataSource: DataSourceTrangThai
-      })
+        field: 'idNganhTrungTuyen',
+        label: 'Ngành trúng tuyển',
+        service: this._DM_NganhService,
+      }),
+      new ColumnSchema({
+        field: 'tongDiem',
+        label: 'Tổng điểm'
+      }),
+      new ColumnSchema({
+        field: 'idHtTuyenSinh',
+        label: 'Hình thức tuyển sinh',
+        service: this._DM_HtTuyenSinhService,
+
+      }),
+      new ColumnSchema({
+        field: 'idDotNhapHoc',
+        label: 'Đợt nhập học',
+        service: this._dm_DotNhapHocService,
+      }),
+      new ColumnSchema({
+        field: 'idDoiTuongDaoTao',
+        label: 'Đối tượng đào tạo',
+        service: this._DM_DoiTuongDaoTaoService,
+
+      }),
+      new ColumnSchema({
+        field: 'soQD',
+        label: 'Số quyết định'
+      }),
+      new ColumnSchema({
+        field: 'maSV',
+        label: 'Mã sinh viên',
+      }),
+
     ];
     super.ngOnInit();
-  }
-
-  override async modifyGridInfo(gridInfo: GridInfo) {
-    if (this.defaultSetting) {
-      if (this.defaultSetting.idHeDaoTao) {
-        gridInfo.filters.push(this.newFilter('idHeDaoTao', Operator.equal, this.defaultSetting.idHeDaoTao));
-      }
-    }
   }
 }
