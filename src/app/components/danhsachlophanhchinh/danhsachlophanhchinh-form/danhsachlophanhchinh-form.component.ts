2d5bf29797e2006137da9cb3a83121fc759e5aaf
lananhhoang88 <67369919+lananhhoang88@users.noreply.github.com>
2022-06-09 07:15:01 +0700

    dslhc

--- src/app/components/danhsachlophanhchinh/danhsachlophanhchinh-form/danhsachlophanhchinh-form.component.ts
+++ src/app/components/danhsachlophanhchinh/danhsachlophanhchinh-form/danhsachlophanhchinh-form.component.ts
@@ -2,6 +2,10 @@ import { Component, Injector, Input, OnInit } from '@angular/core';
 import { TextAreaControlSchema, TextControlSchema, DropdownControlSchema } from 'src/app/shared/models/schema';
 import { FormBase } from '../../../shared/base-class/form-base';
 import { DM_HeDaoTaoService } from '../../dm-hedaotao/services/dm-hedaotao.service';
+import { DM_KhoaHocService } from '../../dm-khoahoc/services/dm-khoahoc.service';
+import { DM_KhoaVienService } from '../../dm-khoavien/services/dm-khoavien.service';
+import { DM_NganhService } from '../../dm-nganh/services/dm-nganh.service';
+import { HoSoCanBoService } from '../../user/services/hosocanbo';
 import { DanhSachLopHanhChinhService } from '../services/danhsachlophanhchinh.service';
 
 @Component({
@@ -14,6 +18,10 @@ export class DanhSachLopHanhChinhFormComponent extends FormBase implements OnIni
     injector: Injector,
     private _DanhSachLopHanhChinhService: DanhSachLopHanhChinhService,
     private _dm_HeDaoTaoService: DM_HeDaoTaoService,
+    private _dm_KhoaHocService: DM_KhoaHocService,
+    private _dm_KhoaVienService: DM_KhoaVienService,
+    private _dm_NganhService: DM_NganhService,
+    private _HoSoCanBoService: HoSoCanBoService,
   ) {
     super(injector);
   }
@@ -21,6 +29,30 @@ export class DanhSachLopHanhChinhFormComponent extends FormBase implements OnIni
   override ngOnInit(): void {
     this.setting.service = this._DanhSachLopHanhChinhService;
     this.setting.schema = [
+      new DropdownControlSchema({
+        field: 'idHeDaoTao',
+        label: 'Hệ đào tạo',
+        required: true,
+        service: this._dm_HeDaoTaoService
+      }),
+      new DropdownControlSchema({
+        field: 'idKhoaHoc',
+        label: 'Khóa học',
+        required: true,
+        service: this._dm_KhoaHocService
+      }),
+      new DropdownControlSchema({
+        field: 'idKhoaVien',
+        label: 'Khoa/Viện',
+        required: true,
+        service: this._dm_KhoaVienService
+      }),
+      new DropdownControlSchema({
+        field: 'idNganh',
+        label: 'Ngành',
+        required: true,
+        service: this._dm_NganhService
+      }),
       new TextControlSchema({
         field: 'ma',
         label: 'Mã',
@@ -29,19 +61,21 @@ export class DanhSachLopHanhChinhFormComponent extends FormBase implements OnIni
       }),
       new TextControlSchema({
         field: 'ten',
-        label: 'Tên đối tượng',
+        label: 'Tên lớp',
         required: true,
         width: 6
       }),
+
       new DropdownControlSchema({
-        field: 'idHeDaoTao',
-        label: 'Hệ đào tạo',
-        required: true,
-        service: this._dm_HeDaoTaoService
+        field: 'idGVCN',
+        label: 'Giáo viên chủ nhiệm',
+        service: this._HoSoCanBoService
       }),
-      new TextControlSchema({
-        field: 'soKyHieu',
-        label: 'Số ký hiệu'
+      new DropdownControlSchema({
+        field: 'idCVHT',
+        label: 'Cố vấn học tập',
+        service: this._HoSoCanBoService,
+        multiple: true
       })
     ];
   }
--- src/app/components/danhsachlophanhchinh/danhsachlophanhchinh.component.ts
+++ src/app/components/danhsachlophanhchinh/danhsachlophanhchinh.component.ts
@@ -3,6 +3,10 @@ import { ColumnSchema, DropdownControlSchema } from '../../shared/models/schema'
 import { ListBase } from '../../shared/base-class/list-base';
 import { DanhSachLopHanhChinhService } from './services/danhsachlophanhchinh.service';
 import { DM_HeDaoTaoService } from '../dm-hedaotao/services/dm-hedaotao.service';
+import { DM_KhoaHocService } from '../dm-khoahoc/services/dm-khoahoc.service';
+import { DM_KhoaVienService } from '../dm-khoavien/services/dm-khoavien.service';
+import { DM_NganhService } from '../dm-nganh/services/dm-nganh.service';
+import { HoSoCanBoService } from '../user/services/hosocanbo';
 @Component({
   selector: 'danhsachlophanhchinh',
   templateUrl: './danhsachlophanhchinh.component.html',
@@ -12,7 +16,11 @@ export class DanhSachLopHanhChinhComponent extends ListBase implements OnInit {
   constructor(
     injector: Injector,
     private _DanhSachLopHanhChinhService: DanhSachLopHanhChinhService,
-    private _dm_HeDaoTaoService: DM_HeDaoTaoService
+    private _dm_HeDaoTaoService: DM_HeDaoTaoService,
+    private _dm_KhoaHocService: DM_KhoaHocService,
+    private _dm_KhoaVienService: DM_KhoaVienService,
+    private _dm_NganhService: DM_NganhService,
+    private _HoSoCanBoService: HoSoCanBoService,
   ) {
     super(injector);
   }
@@ -20,16 +28,19 @@ export class DanhSachLopHanhChinhComponent extends ListBase implements OnInit {
   override ngOnInit(): void {
     this.setting.objectName = 'lớp hành chính';
     this.setting.service = this._DanhSachLopHanhChinhService;
+    this.setting.popupSize.width = 1100;
+    this.setting.popupSize.height = 500;
     this.setting.cols = [
       new ColumnSchema({
         field: 'ma',
         label: 'Mã',
-        width: '300px',
+        width: '200px',
         fullTextSearch: true
       }),
       new ColumnSchema({
         field: 'ten',
-        label: 'Tên hệ',
+        label: 'Tên lớp',
+        width: '300px',
         fullTextSearch: true
       }),
       new ColumnSchema({
@@ -38,9 +49,24 @@ export class DanhSachLopHanhChinhComponent extends ListBase implements OnInit {
         service: this._dm_HeDaoTaoService
       }),
       new ColumnSchema({
-        field: 'soKyHieu',
-        fullTextSearch: true,
-        label: 'Số ký hiệu'
+        field: 'idKhoaHoc',
+        label: 'Khóa học',
+        service: this._dm_KhoaHocService
+      }),
+      new ColumnSchema({
+        field: 'idKhoaVien',
+        label: 'Khoa/Viện',
+        service: this._dm_KhoaHocService
+      }),
+      new ColumnSchema({
+        field: 'idNganh',
+        label: 'Ngành',
+        service: this._dm_NganhService
+      }),
+      new ColumnSchema({
+        field: 'siSo',
+        width: '150px',
+        label: 'Sĩ số'
       }),
     ];
     super.ngOnInit();
