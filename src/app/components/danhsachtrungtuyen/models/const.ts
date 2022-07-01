6406fb9f8a199de805722e77b8ef41bbdab5a25c
dovanduy91 <duydv@trinam.vn>
2022-07-01 11:58:51 +0700

    Sửa phần quyết định học tập

--- src/app/components/danhsachtrungtuyen/models/const.ts
+++ src/app/components/danhsachtrungtuyen/models/const.ts
@@ -1,8 +1,8 @@
 import { EnumTrangThaiHS } from "./enums";
 
 export const DataSourceTrangThaiHoSo = [
-  { _id: EnumTrangThaiHS.TT1, ten: 'Nộp đủ' },
-  { _id: EnumTrangThaiHS.TT2, ten: 'Nộp thiếu' },
-  { _id: EnumTrangThaiHS.TT3, ten: 'Chưa nộp' },
-  { _id: EnumTrangThaiHS.TT4, ten: 'Đã rút' },
+  { _id: EnumTrangThaiHS.NOP_DU, ten: 'Nộp đủ' },
+  { _id: EnumTrangThaiHS.NOP_THIEU, ten: 'Nộp thiếu' },
+  { _id: EnumTrangThaiHS.CHUA_NOP, ten: 'Chưa nộp' },
+  { _id: EnumTrangThaiHS.DA_RUT, ten: 'Đã rút' },
 ];
